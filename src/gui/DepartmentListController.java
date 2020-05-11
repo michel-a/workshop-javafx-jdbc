d8b7f47131171967929e0c45626ff3c3c059229c
michel-a <malvesdesign@hotmail.com>
2020-05-11 20:43:10 -0300

    DepartmentList controller

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,51 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+public class DepartmentListController implements Initializable {
+
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	private Button btNew;
+	
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		initializaNodes();
+	}
+
+	private void initializaNodes() {
+		// Padrão do javafx para iniciar o comportamento das colunas
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		// getWindow() = pega a referência da janela, window é uma superclasse de stage e por isso é necessário fazer o downcasting 
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		// através desse comando, o taleView vai acompanhar a altura da janela
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+}
